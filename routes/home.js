fcedc29f1c23c98395bc7fb734cd578249e8552a
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2019-10-08 19:48:55 -0300

    Controlador y ruta
    Agregada controlador para la pagina y su ruta correspondiente

--- routes/home.js
+++ routes/home.js
@@ -5,4 +5,7 @@ const homeController = require('../controllers/homeController');
 //Al recibir request de home, usa homeController
 router.get('/', homeController.getHomePage);
 
+//Al recibir la URL /aboutUs, llama al controlador homeController y ahi llama a la funcion getAboutUsPage
+router.get('/aboutUs', homeController.getAboutUsPage);
+
 module.exports = router;
